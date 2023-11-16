592723f797b5f1e1e4b3c11d13b1a5c417f7c090
isthisnull <letscodewithzanjiri@hotmail.com>
2023-11-16 20:32:14 +0300

    wrong logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@ function App() {
   return (
     <>
       <div className="container">
+        <div className="pro">Profile</div>
         <div className="center">
-          <div className="left">abcddddd</div>
+          <div className="centerContent">
+            <img src="assets/back.webp" alt="koooooni" className="backPic" />
+          </div>
+          <div className="left">
+            sss{/* <div className="pro">Profile</div> */}
+          </div>
           <div className="right"></div>
         </div>
       </div>
