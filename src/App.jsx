cdd05ec0fb286cd5fbc0df24531e4e6e5286db2c
isthisnull <letscodewithzanjiri@hotmail.com>
2023-11-18 00:17:10 +0300

    11

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,39 @@ import "./index.css";
 function App() {
   return (
     <>
-      <div className="container">
-        <div className="left">
-          <p>fsadcdscsdcsdcccsd</p>
-          <p>casdcascacdsasdcsadsad</p>
-          <p>sac</p>
-          <p>casdcsad</p>
-          <p>fsadcsd</p>
-          <p>casdcsad</p>
-          <p>fsadcsd</p>
-          <p>casdcsad</p>
-        </div>
-
-        <div className="center">
-          {/* <img src="assets/back.webp" alt="koooooni" className="backPic" /> */}
-          <p>fsadcsd</p>
-          <p>casdcsad</p>
-          <p>fsadcsd</p>
-          <p>casdcsad</p>
-          <p>fsadcsd</p>
-          <p>casdcsad</p>
-          <p>fsadcsd</p>
-          <p>casdcsad</p>
-        </div>
-        <div className="right">
-          <p>fsadcsd</p>
-          <p>casdcsad</p>
-          <p>fsadcsd</p>
-          <p>casdcsad</p>
-          <p>fsadcsd</p>
-          <p>casdcsad</p>
-          <p>fsadcsd</p>
-          <p>casdcsad</p>
+      <div className="containerC">
+        <div className="container">
+          <div className="left">
+            <p>fsadcdscsdcsdcccsd</p>
+            <p>casdcascacdsasdcsadsad</p>
+            <p>sac</p>
+            <p>casdcsad</p>
+            <p>fsadcsd</p>
+            <p>casdcsad</p>
+            <p>fsadcsd</p>
+            <p>casdcsad</p>
+          </div>
+          <div className="center">
+            {/* <img src="assets/back.webp" alt="koooooni" className="backPic" /> */}
+            <p>fsadcsd</p>
+            <p>casdcsad</p>
+            <p>fsadcsd</p>
+            <p>casdcsad</p>
+            <p>fsadcsd</p>
+            <p>casdcsad</p>
+            <p>fsadcsd</p>
+            <p>casdcsad</p>
+          </div>
+          <div className="right">
+            <p>fsadcsd</p>
+            <p>casdcsad</p>
+            <p>fsadcsd</p>
+            <p>casdcsad</p>
+            <p>fsadcsd</p>
+            <p>casdcsad</p>
+            <p>fsadcsd</p>
+            <p>casdcsad</p>
+          </div>
         </div>
       </div>
     </>
