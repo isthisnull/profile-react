72332b9a7cf8132d93af8aa97974eb1e5e449664
isthisnull <letscodewithzanjiri@hotmail.com>
2023-11-17 15:15:09 +0300

    logic done

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,29 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="left"></div>
+        <div className="left">
+          <p>fsadcsd</p>
+          <p>casdcsad</p>
+          <p>fsadcsd</p>
+          <p>casdcsad</p>
+          <p>fsadcsd</p>
+          <p>casdcsad</p>
+          <p>fsadcsd</p>
+          <p>casdcsad</p>
+        </div>
         <div className="center">
           <img src="assets/back.webp" alt="koooooni" className="backPic" />
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <p>fsadcsd</p>
+          <p>casdcsad</p>
+          <p>fsadcsd</p>
+          <p>casdcsad</p>
+          <p>fsadcsd</p>
+          <p>casdcsad</p>
+          <p>fsadcsd</p>
+          <p>casdcsad</p>
+        </div>
       </div>
     </>
   );
