73ef325abecb6c4c1bb8c5809e603a50a6736234
isthisnull <letscodewithzanjiri@hotmail.com>
2023-11-23 00:48:02 +0300

    1px

--- src/App.jsx
+++ src/App.jsx
@@ -32,20 +32,28 @@ function App() {
               </div>
             </div>
             <div className="idFlw">
-              <div className="nik">Tasmetime</div>
-              <div className="verify">
-                <img src="assets\verify.png" className="verifyMark" />
+              <div className="nikVerify">
+                <div className="nik">Tasmetime</div>
+                <div className="verify">
+                  <img src="assets\verify.png" className="verifyMark" />
+                </div>
               </div>
               <div className="buttons">
                 <button className="message"></button>
                 <button className="notif"></button>
-                <button className="flw"></button>
+                <button className="flw">Follow</button>
               </div>
             </div>
             <div className="id">@tasmetime</div>
-            <div className="joined">Joined November 2023</div>
+            <div className="joined">
+              <img src="assets\verify.png" alt="" className="dateIcon" />
+              <p>Joined November 2023</p>
+            </div>
             <div className="followers"></div>
-            <div className="content"></div>
+            <div className="content">
+              <div className="followers">Followers: 11</div>
+              <div className="following">Following: 11</div>
+            </div>
           </div>
           <div className="right">
             <p>fsadcsd</p>
